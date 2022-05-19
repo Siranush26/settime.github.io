819e357ca0db89bc7d6b6e414fcdfd63bc790282
Siranush26 <siranush.tsaturyan.dwin@gmail.com>
2022-05-19 14:24:47 +0400

    "before after"

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 let button1=document.getElementById("button1");
 let button2=document.getElementById("button2");
-let element=document.querySelector(".heart");
-let container=document.getElementsByClassName("container")
+let element=document.querySelector(".heart");           
+let circle=document.querySelector("#circle");
+let container=document.getElementsByClassName("container");
+
+//let before =window.getComputedStyle(element, ':before') 
+//console.log(before.backgroundColor)
+
 //console.log(getComputedStyle(document.querySelector('#heart'), ':before').getPropertyValue('content'));
-let styleElem = document.head.appendChild(document.createElement("style"));
 
-styleElem.innerHTML = ".heart:before{background:blue}";
+//let styleElem = document.head.appendChild(document.createElement("style"));
+//styleElem.innerHTML = ".heart:before{background:blue}";
 
+
+//let elem_after = getComputedStyle(element, ':after')
+//let elembefore =getComputedStyle(element, ':before')
 
 
 
 button1.addEventListener("click",()=>{
     let intervalStop = setInterval(()=>{
     let letters = '0123456789ABCDEF';
-    let color = "#";
+    let colorss = "#";
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      colorss += letters[Math.floor(Math.random() * 16)];
     }
-   
-    element.style.background=color
-    circle.style.background=color
+    
+    circle.style.background=colorss
+    element.style.setProperty('--beforeback','blue')
+    element.style.background=colorss
+    //elem_after.style.background = colorss 
+    //elem_before.style.background = colorss 
+  
   },1000)
 
 
